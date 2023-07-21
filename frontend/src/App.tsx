046726de1122e99f5fa78679bc051c3b39d5dc30
minhtrannhat <minh@minhtrannhat.com>
2023-07-21 18:23:34 -0400

    Fix(Frontend): Set maxWidth to md
    - This will make sure Material UI's Container will not expand to fill
      the entire screen and instead just stop expanding at around 960px
      (which is `md`).

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+// Roboto font and its weights
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+// Theming
+import ThemeProvider from "./ThemeProvider";
+
+// Material UI stuffs
+import Container from "@mui/material/Container";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Container maxWidth="md"></Container>
+    </ThemeProvider>
   );
 }
 
